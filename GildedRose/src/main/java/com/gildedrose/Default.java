fd6f2741d3885fc0fa435a8e168a3d8625a24020
korban2u <ryankorban2004@gmail.com>
2026-03-12 09:57:19 +0100

    add Default.java

---
+++ GildedRose/src/main/java/com/gildedrose/Default.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+public class Default extends Item{
+    public Default(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void update(){
+        if (this.quality > 0) {
+            this.quality--;
+        }
+        this.sellIn--;
+
+        if (this.sellIn < 0 && this.quality > 0) {
+            this.quality--;
+        }
+    }
+}
