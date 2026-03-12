ea622ef38046769258d9b2be485822ff0be830b2
korban2u <ryankorban2004@gmail.com>
2026-03-12 09:51:20 +0100

    add AgedBrie.java

---
+++ GildedRose/src/main/java/com/gildedrose/AgedBrie.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+public class AgedBrie extends Item{
+    public AgedBrie(int sellIn, int quality) {
+        super("Aged Brie", sellIn, quality);
+    }
+
+    public void update(){
+        if (this.quality < 50) {
+            this.quality++;
+        }
+        this.sellIn--;
+
+        if (this.sellIn < 0 && this.quality < 50) {
+            this.quality++;
+        }
+    }
+
+}
