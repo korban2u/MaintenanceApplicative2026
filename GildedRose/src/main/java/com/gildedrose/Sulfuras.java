8b280275d02a30276922c23552aa3045464deeb3
korban2u <ryankorban2004@gmail.com>
2026-03-12 10:19:09 +0100

    fix Sulfuras.java

--- GildedRose/src/main/java/com/gildedrose/Sulfuras.java
+++ GildedRose/src/main/java/com/gildedrose/Sulfuras.java
@@ -1,8 +1,8 @@
 package com.gildedrose;
 
 public class Sulfuras extends ItemWrapper {
-    public Sulfuras(String name, int sellIn, int quality) {
-        super(name, sellIn, quality);
+    public Sulfuras(int sellIn, int quality) {
+        super("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
 
     public void update(){
