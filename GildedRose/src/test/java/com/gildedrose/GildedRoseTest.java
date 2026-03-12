51679e3bbda1a13f8a6383af08d2dc218baa88f2
korban2u <ryankorban2004@gmail.com>
2026-03-12 10:18:54 +0100

    maj tests

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        ItemWrapper[] items = new ItemWrapper[] { new Default("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
@@ -16,7 +16,7 @@ class GildedRoseTest {
 
     @Test
     void testNormalItemBeforeSellDate() {
-        Item[] items = new Item[] { new Item("foo", 10, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Default("foo", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -25,7 +25,7 @@ class GildedRoseTest {
 
     @Test
     void testNormalItemOnOrAfterSellDate() {
-        Item[] items = new Item[] { new Item("foo", 0, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Default("foo", 0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
@@ -34,7 +34,7 @@ class GildedRoseTest {
 
     @Test
     void testNormalItemQualityNeverNegative() {
-        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        ItemWrapper[] items = new ItemWrapper[] { new Default("foo", 10, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -43,7 +43,7 @@ class GildedRoseTest {
 
     @Test
     void testAgedBrieBeforeSellDate() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new AgedBrie(10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -52,7 +52,7 @@ class GildedRoseTest {
 
     @Test
     void testAgedBrieAfterSellDate() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new AgedBrie( 0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
@@ -61,7 +61,7 @@ class GildedRoseTest {
 
     @Test
     void testAgedBrieQualityNeverExceedsFifty() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        ItemWrapper[] items = new ItemWrapper[] { new AgedBrie(10, 50) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -70,7 +70,7 @@ class GildedRoseTest {
 
     @Test
     void testSulfurasNeverChanges() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        ItemWrapper[] items = new ItemWrapper[] { new Sulfuras(10, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(10, app.items[0].sellIn);
@@ -79,7 +79,7 @@ class GildedRoseTest {
 
     @Test
     void testSulfurasAfterSellDateNeverChanges() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 80) };
+        ItemWrapper[] items = new ItemWrapper[] { new Sulfuras(-1, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
@@ -88,7 +88,7 @@ class GildedRoseTest {
 
     @Test
     void testBackstagePassesFarAway() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Backstage(15, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(14, app.items[0].sellIn);
@@ -97,7 +97,7 @@ class GildedRoseTest {
 
     @Test
     void testBackstagePassesTenDaysOrLess() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Backstage( 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
@@ -106,7 +106,7 @@ class GildedRoseTest {
 
     @Test
     void testBackstagePassesFiveDaysOrLess() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Backstage(5, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(4, app.items[0].sellIn);
@@ -115,7 +115,7 @@ class GildedRoseTest {
 
     @Test
     void testBackstagePassesAfterConcert() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        ItemWrapper[] items = new ItemWrapper[] { new Backstage(0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(-1, app.items[0].sellIn);
@@ -124,7 +124,7 @@ class GildedRoseTest {
 
     @Test
     void testBackstagePassesMaxQualityCap() {
-        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49) };
+        ItemWrapper[] items = new ItemWrapper[] { new Backstage(5, 49) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(4, app.items[0].sellIn);
@@ -133,13 +133,13 @@ class GildedRoseTest {
 
     @Test
     void testItemToString() {
-        Item item = new Item("Elixir of the Mongoose", 5, 7);
+        Item item = new Default("Elixir of the Mongoose", 5, 7);
         assertEquals("Elixir of the Mongoose, 5, 7", item.toString());
     }
 
     @Test
     void testEmptyArrayDoesNotCrash() {
-        Item[] items = new Item[] {};
+        ItemWrapper[] items = new ItemWrapper[] {};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(0, app.items.length);
@@ -147,7 +147,7 @@ class GildedRoseTest {
 
     @Test
     void testQualityUpper50(){
-        Item[] items = new Item[] { new Item("Aged Brie", -5, 52) };
+        ItemWrapper[] items = new ItemWrapper[] { new AgedBrie(-5, 52) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(52,app.items[0].quality);
